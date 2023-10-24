528ab68193a8eceef27e639808ecf703ab96ee4b
twiglauer <twigliu@163.com>
2023-10-25 07:13:40 +0800

    discover

--- proj2-music-app/src/pages/discover/index.js
+++ proj2-music-app/src/pages/discover/index.js
@@ -1,8 +1,28 @@
 import React, { memo } from 'react'
-
-const Discover = memo(() => {
+import {
+  DiscoverWrapper,
+  TopMenu
+} from './style'
+import { dicoverMenu } from '@/common/local-data'
+import { NavLink } from 'react-router-dom'
+import { renderRoutes } from 'react-router-config'
+const Discover = memo((props) => {
+  const { route } = props
   return (
-    <div>Discover</div>
+    <DiscoverWrapper>
+      <div className='top'>
+        <TopMenu className='wrap-v1'>
+          { 
+            dicoverMenu.map(ele => (
+              <div className='item' key={ele.title}>
+                <NavLink  to={ele.link}>{ele.title}</NavLink>
+              </div>
+            ))
+          }
+        </TopMenu>
+      </div>
+      {renderRoutes(route.routes)}
+    </DiscoverWrapper>
   )
 })
 
--- proj2-music-app/src/router/index.js
+++ proj2-music-app/src/router/index.js
@@ -1,11 +1,68 @@
+import React from 'react';
+import { Redirect } from 'react-router-dom';
+
 import Discover from '@/pages/discover';
 import Friend from '@/pages/friend';
 import Mine from '@/pages/mine';
+
+import Album from '@/pages/discover/c-pages/album'
+import Artist from '@/pages/discover/c-pages/artist'
+import Djradio from '@/pages/discover/c-pages/djradio'
+import Ranking from '@/pages/discover/c-pages/ranking'
+import Recommend from '@/pages/discover/c-pages/recommend'
+import Songs from '@/pages/discover/c-pages/songs'
+import Player from '@/pages/discover/c-pages/player'
 const routes = [
    {
     path:'/',
     exact: true,
-    component:Discover
+    // TODO: 这里还可以写成render函数
+    render:()=>{
+      <Redirect to="/discover" />
+    }
+   },
+   {
+      path:'/discover',
+      component: Discover,
+      // TODO: 子路由
+      routes: [
+        {
+          path: "/discover",
+          exact: true,
+          render: () => (
+            <Redirect to="/discover/recommend"/>
+          )
+        },
+        {
+          path: "/discover/recommend",
+          component: Recommend
+        },
+        {
+          path: "/discover/ranking",
+          component: Ranking
+        },
+        {
+          path: "/discover/songs",
+          component: Songs
+        },
+        {
+          path: "/discover/djradio",
+          exact: true,
+          component: Djradio
+        },
+        {
+          path: "/discover/artist",
+          component: Artist
+        },
+        {
+          path: "/discover/album",
+          component: Album
+        },
+        {
+          path: "/discover/player",
+          component: Player
+        }
+      ]
    },
    {
     path:'/friend',
