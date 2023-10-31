b830da347a20c8cb255f5f5241ac40284500bf01
twiglauer <twigliu@163.com>
2023-10-31 08:37:09 +0800

    hot

---
+++ proj2-music-app/src/pages/discover/c-pages/recommend/c-cpns/hot-recommend/index.js
@@ -0,0 +1,9 @@
+import React, { memo } from 'react'
+
+const HotRecommend = memo(() => {
+  return (
+    <div>HotRecommend</div>
+  )
+})
+
+export default HotRecommend
--- proj2-music-app/src/pages/discover/c-pages/recommend/index.js
+++ proj2-music-app/src/pages/discover/c-pages/recommend/index.js
@@ -1,9 +1,13 @@
 import React, { memo, useEffect } from 'react'
 
 import {
-    RecommendWrapper
+    RecommendWrapper,
+    Content,
+    RecommendLeft,
+    RecommendRight
 } from './style'
 import TopBanner from './c-cpns/top-banner'
+import HotRecommend from './c-cpns/hot-recommend'
 const Recommend = memo(() => {
 
     
@@ -11,6 +15,12 @@ const Recommend = memo(() => {
     return (
         <RecommendWrapper>
             <TopBanner />
+            <Content>
+                <RecommendLeft>
+                  <HotRecommend></HotRecommend>
+                </RecommendLeft>
+                <RecommendRight></RecommendRight>
+            </Content>
         </RecommendWrapper>
     )
 })
