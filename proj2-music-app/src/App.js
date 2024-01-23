a4c00cdb13ce3dc935fceb9aceb2eabdb9aaffe3
twiglau <twigliu@hotmail.com>
2024-01-23 11:13:10 +0800

    修改内容g

--- proj2-music-app/src/App.js
+++ proj2-music-app/src/App.js
@@ -1,23 +1,23 @@
-import React, { memo } from 'react'
-import { renderRoutes } from 'react-router-config'
-import { HashRouter } from 'react-router-dom'
-import { Provider } from 'react-redux'
-import routes from './router'
-import store from './store'
+import React, { memo } from "react";
+import { renderRoutes } from "react-router-config";
+import { HashRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import routes from "./router";
+import store from "./store";
 
-import AppHeader from 'components/app-header'
-import AppFooter from 'components/app-footer'
-import AppPlayerBar from './pages/player/app-player-bar'
+import AppHeader from "components/app-header";
+import AppFooter from "components/app-footer";
+import AppPlayerBar from "./pages/player/app-player-bar";
 
 export default memo(function App() {
-    return (
-      <Provider store={store}>
-        <HashRouter>
-          <AppHeader />
-          {renderRoutes(routes)}
-          <AppFooter />
-          <AppPlayerBar />
-        </HashRouter>
-      </Provider>
-    )
-})
+  return (
+    <Provider store={store}>
+      <HashRouter>
+        <AppHeader />
+        {renderRoutes(routes)}
+        <AppFooter />
+        <AppPlayerBar />
+      </HashRouter>
+    </Provider>
+  );
+});
--- proj2-music-app/src/router/index.js
+++ proj2-music-app/src/router/index.js
@@ -1,19 +1,54 @@
-import React from 'react';
-import { Redirect } from 'react-router-dom';
+import React, { Suspense } from "react";
+import { Redirect } from "react-router-dom";
 
+// Using React.lazy() => Code Splitting
+const Discover = React.lazy((_) => import("@/pages/discover"));
+// Somewhere in your application
+Discover.preload();
 
-const Discover = React.lazy(_ => import('@/pages/discover'))
-const Friend = React.lazy(_ => import('@/pages/friend'))
-const Mine = React.lazy(_ => import('@/pages/mine'))
+// 使用 import() 动态导入
+const Friend = React.lazy((_) => import("@/pages/friend"));
 
+const App = () => {
+  return (
+    <Router>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Switch>
+          <Route path="/" exact component={Discover} />
+          <Route path="/friend" component={Friend} />
+        </Switch>
+      </Suspense>
+    </Router>
+  );
+};
+// // Fallback Component with Suspense
+// const App = () => {
+//   return (
+//     <React.Suspense fallback={<div>Loading...</div>}>
+//       <Discover />
+//     </React.Suspense>
+//   );
+// };
 
-const Album = React.lazy(_ => import('@/pages/discover/c-pages/album'))
-const Artist = React.lazy(_ => import('@/pages/discover/c-pages/artist'))
-const Djradio = React.lazy(_ => import('@/pages/discover/c-pages/djradio'))
-const Ranking = React.lazy(_ => import('@/pages/discover/c-pages/ranking'))
-const Recommend = React.lazy(_ => import('@/pages/discover/c-pages/recommend'))
-const Songs = React.lazy(_ => import('@/pages/discover/c-pages/songs'))
-const Player = React.lazy(_ => import('@/pages/player'))
+// // Frined.js
+// export const Friend = /** */
+
+// // Lazily load the named export
+// const LazyFriendComponent = React.lazy((_) =>
+//   import("@/pages/friend").then((module) => ({ default: module.Friend }))
+// );
+
+const Mine = React.lazy((_) => import("@/pages/mine"));
+
+const Album = React.lazy((_) => import("@/pages/discover/c-pages/album"));
+const Artist = React.lazy((_) => import("@/pages/discover/c-pages/artist"));
+const Djradio = React.lazy((_) => import("@/pages/discover/c-pages/djradio"));
+const Ranking = React.lazy((_) => import("@/pages/discover/c-pages/ranking"));
+const Recommend = React.lazy((_) =>
+  import("@/pages/discover/c-pages/recommend")
+);
+const Songs = React.lazy((_) => import("@/pages/discover/c-pages/songs"));
+const Player = React.lazy((_) => import("@/pages/player"));
 // import Discover from '@/pages/discover';
 // import Friend from '@/pages/friend';
 // import Mine from '@/pages/mine';
@@ -26,65 +61,63 @@ const Player = React.lazy(_ => import('@/pages/player'))
 // import Songs from '@/pages/discover/c-pages/songs'
 // import Player from '@/pages/player'
 const routes = [
-   {
-    path:'/',
+  {
+    path: "/",
     exact: true,
     // TODO: 这里还可以写成render函数
-    render:()=>{
-      <Redirect to="/discover" />
-    }
-   },
-   {
-      path:'/discover',
-      component: Discover,
-      // TODO: 子路由
-      routes: [
-        {
-          path: "/discover",
-          exact: true,
-          render: () => (
-            <Redirect to="/discover/recommend"/>
-          )
-        },
-        {
-          path: "/discover/recommend",
-          component: Recommend
-        },
-        {
-          path: "/discover/ranking",
-          component: Ranking
-        },
-        {
-          path: "/discover/songs",
-          component: Songs
-        },
-        {
-          path: "/discover/djradio",
-          exact: true,
-          component: Djradio
-        },
-        {
-          path: "/discover/artist",
-          component: Artist
-        },
-        {
-          path: "/discover/album",
-          component: Album
-        },
-        {
-          path: "/discover/player",
-          component: Player
-        }
-      ]
-   },
-   {
-    path:'/friend',
-    component:Friend
-   },
-   {
-    path:'/mine',
-    component:Mine
-   },
+    render: () => {
+      <Redirect to="/discover" />;
+    },
+  },
+  {
+    path: "/discover",
+    component: Discover,
+    // TODO: 子路由
+    routes: [
+      {
+        path: "/discover",
+        exact: true,
+        render: () => <Redirect to="/discover/recommend" />,
+      },
+      {
+        path: "/discover/recommend",
+        component: Recommend,
+      },
+      {
+        path: "/discover/ranking",
+        component: Ranking,
+      },
+      {
+        path: "/discover/songs",
+        component: Songs,
+      },
+      {
+        path: "/discover/djradio",
+        exact: true,
+        component: Djradio,
+      },
+      {
+        path: "/discover/artist",
+        component: Artist,
+      },
+      {
+        path: "/discover/album",
+        component: Album,
+      },
+      {
+        path: "/discover/player",
+        component: Player,
+      },
+    ],
+  },
+  {
+    path: "/friend",
+    component: Friend,
+  },
+  {
+    path: "/mine",
+    component: Mine,
+  },
 ];
 
 export default routes;
