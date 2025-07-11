367aa36d9d3b18a808f410dc65e0afc528a32da0
twiglau <twigliu@hotmail.com>
2025-07-11 14:55:29 +0800

    feat: react19

--- react19feature/babel.config.cjs
+++
@@ -1,5 +0,0 @@
-const ReactCompilerConfig = require("babel-plugin-react-compiler");
-
-module.exports = {
-  plugins: [["babel-plugin-react-compiler", ReactCompilerConfig]],
-};
--- react19feature/src/form-action2/SubmitButton.jsx
+++
@@ -1,9 +0,0 @@
-import { useFormStatus } from 'react-dom'
-import Button from '../Button'
-
-export default function SubmitButton({children}) {
-    const { pending } = useFormStatus()
-    return (
-        <Button primary disabled={pending}>{children}{pending ? '...':''}</Button>
-    )
-}
